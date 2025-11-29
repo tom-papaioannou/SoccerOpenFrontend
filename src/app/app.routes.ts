866d932f7c70f2f2019d641d2b477840463fc044
Tom Papaioannou <tom-papaioannou@outlook.com>
2025-11-29 13:46:52 +0200

    Add tournaments page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { Login } from './components/login/login';
 import { authenticationGuard } from './guards/authentication.guard';
+import { Tournaments } from './components/tournaments/tournaments';
 
 export const routes: Routes = [
     { path: 'home', canActivate: [authenticationGuard], component: Home },
     // { path: 'error', component: Home },
     { path: 'login', component: Login },
+    { path: 'tournaments', component: Tournaments },
     { path: '**', redirectTo: '/home' },
 ];
---
+++ src/app/components/tournaments/tournaments.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-tournaments',
+  imports: [MatCardModule],
+  templateUrl: './tournaments.html',
+  styleUrl: './tournaments.css'
+})
+export class Tournaments {
+
+}
