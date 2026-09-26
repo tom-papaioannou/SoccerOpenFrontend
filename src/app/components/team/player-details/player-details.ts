f8a138edb8edd485b72ab8bc44097c658ad75cac
Tom Papaioannou <tom-papaioannou@outlook.com>
2026-09-26 14:43:55 +0300

    Feature/player leg ratings (#95)
    * Display player leg ratings
    
    * Update ui for right and left foot stats

--- src/app/components/team/player-details/player-details.ts
+++ src/app/components/team/player-details/player-details.ts
@@ -369,7 +369,9 @@ export class PlayerDetails implements OnInit, OnDestroy, AfterViewChecked {
             { name: 'Acceleration', value: stats.acceleration },
             { name: 'Strength', value: stats.strength },
             { name: 'Jumping', value: stats.jumping },
-            { name: 'Stamina', value: stats.stamina }
+            { name: 'Stamina', value: stats.stamina },
+            { name: 'Right Foot', value: stats.rightLegRating },
+            { name: 'Left Foot', value: stats.leftLegRating }
           ]
         },
         {
